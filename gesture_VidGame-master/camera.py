4286ed53b597accaa3e336c9ee6b07dd3236af96
Rafsan Jany <59754354+RafsanJany-44@users.noreply.github.com>
2022-04-14 13:44:34 +0600

    up

--- gesture_VidGame-master/camera.py
+++ gesture_VidGame-master/camera.py
@@ -77,9 +77,9 @@ with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5) a
         if results.multi_hand_landmarks:
             for num, hand in enumerate(results.multi_hand_landmarks):
                 mp_drawing.draw_landmarks(image, hand, mp_hands.HAND_CONNECTIONS,
-                                          mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=4),
-                                          mp_drawing.DrawingSpec(color=(250, 44, 250), thickness=2, circle_radius=2),
-                                          )
+                                    mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=4),
+                                    mp_drawing.DrawingSpec(color=(250, 44, 250), thickness=2, circle_radius=2),
+                                    )
 
                 # Render left or right detection
                 if get_label(num, hand, results):
