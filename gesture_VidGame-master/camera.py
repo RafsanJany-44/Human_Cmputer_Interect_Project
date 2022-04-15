b4c79f37d6250d2e3a18aa50c42e95d8a110cc7b
Rafsan Jany <59754354+RafsanJany-44@users.noreply.github.com>
2022-04-15 22:43:47 +0600

    up

--- gesture_VidGame-master/camera.py
+++ gesture_VidGame-master/camera.py
@@ -10,7 +10,7 @@ mp_drawing = mp.solutions.drawing_utils # taking drwaing utils
 mp_hands = mp.solutions.hands
 joint_list =[[8,5,0]]  # ladbmarks of 8,5,0
 
-def draw_finger_angles(image, results, joint_list):   # this line is drwaing the finger line
+def draw_finger_angles(image, results, joint_list):   # this line is drwaing the finger line and the text in the monitor and calculating angle
     # Loop through hands 
     for hand in results.multi_hand_landmarks:  #building a loop for hand marks
         # Loop through joint sets
@@ -20,14 +20,14 @@ def draw_finger_angles(image, results, joint_list):   # this line is drwaing the
             c = np.array([hand.landmark[joint[2]].x, hand.landmark[joint[2]].y])  # Third coord
 
             radians = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(a[1] - b[1], a[0] - b[0])
-            angle = np.abs(radians * 180.0 / np.pi)
+            angle = np.abs(radians * 180.0 / np.pi) #calculating angle for decision left or right
 
 
             cv2.putText(image, str(round(angle, 2)), tuple(np.multiply(b, [640, 480]).astype(int)),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA) #puting text into the monitor
     return image, angle
 
-def get_label(index, hand, results):
+def get_label(index, hand, results):  #getting the level
     output = None
     for idx, classification in enumerate(results.multi_handedness):
         if classification.classification[0].index == index:
@@ -46,7 +46,7 @@ def get_label(index, hand, results):
     return output
 
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0) #for vedio capturing
 
 with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5) as hands:
     while cap.isOpened():
@@ -79,7 +79,7 @@ with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5) a
                 mp_drawing.draw_landmarks(image, hand, mp_hands.HAND_CONNECTIONS,
                                     mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=4),
                                     mp_drawing.DrawingSpec(color=(250, 44, 250), thickness=2, circle_radius=2),
-                                    )
+                                    ) # drwaing each land mark and lines in hand
 
                 # Render left or right detection
                 if get_label(num, hand, results):
@@ -87,26 +87,26 @@ with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5) a
                     cv2.putText(image, text, coord, cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
 
             # Draw angles to image from joint list
-            image, angle = draw_finger_angles(image, results, joint_list)
-            keyboard = Controller()
+            image, angle = draw_finger_angles(image, results, joint_list) #calculating angel and drwain in image
+            keyboard = Controller() #calling keayboard controler
             if angle<=180:
-                keyboard.press(Key.right)
-                keyboard.release(Key.right)
+                keyboard.press(Key.right) # the main point keay board decision function for right
+                keyboard.release(Key.right)# the main point keay board decision function for left
             else:
                 keyboard.press(Key.left)
                 keyboard.release(Key.left)
 
         # Save our image
         # cv2.imwrite(os.path.join('Output Images', '{}.jpg'.format(uuid.uuid1())), image)
-            cv2.rectangle(image, (0, 0), (355, 73), (214, 44, 53))
-            cv2.putText(image, 'Direction', (15, 12),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+            cv2.rectangle(image, (0, 0), (355, 73), (214, 44, 53)) #ractangle viwing window
+            cv2.putText(image, 'Direction', (15, 12), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)#image text
             cv2.putText(image, "Left" if angle >180 else "Right",
                         (10, 60),
                         cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
-        cv2.imshow('Hand Tracking', image)
+        cv2.imshow('Hand Tracking', image)# showing the image of tracking hand
 
-        if cv2.waitKey(10) & 0xFF == ord('q'):
+        if cv2.waitKey(10) & 0xFF == ord('q'): #termininate key
             break
 
 
